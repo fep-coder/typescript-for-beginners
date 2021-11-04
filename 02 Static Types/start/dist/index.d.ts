099fc0fd192a928d5b0e6fbe686ce4c896964807
voja <voja@gmail.com>
2021-11-04 10:11:44 +0100

    02 Revealing types being used

---
+++ 02 Static Types/start/dist/index.d.ts
@@ -0,0 +1,7 @@
+declare let myVar: any;
+declare function multiply(amount: any): number;
+declare function multiplyTyped(amount: number): number;
+declare let number: number;
+declare let multiplyNumber: number;
+declare let number2: number;
+declare let multiplyNumber2: number;
