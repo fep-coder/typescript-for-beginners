4c1670ec9d59c0ecaadac999a3754454799d9321
voja <voja@gmail.com>
2021-11-05 09:34:23 +0100

    03 Optional and default parameters

--- 03 Functions/start/dist/index.js
+++ 03 Functions/start/dist/index.js
@@ -10,3 +10,15 @@ function params(param1, param2) {
 // const params1 = params(1);
 const params2 = params(1, 2);
 // const params3 = params(1, 2, 3);
+function optional(a, b) {
+    return a + (b || 5);
+}
+let opt = optional(1, 2);
+console.log(opt);
+opt = optional(1);
+console.log(opt);
+function defaultValues(a, b = 5) {
+    return a + b;
+}
+let def = defaultValues(1);
+console.log(def);
