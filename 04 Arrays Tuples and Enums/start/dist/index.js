97ab8da452af3fec930aeb0a4f5e29731029288e
voja <voja@gmail.com>
2021-11-08 09:09:09 +0100

    04 Working with tuples

--- 04 Arrays Tuples and Enums/start/dist/index.js
+++ 04 Arrays Tuples and Enums/start/dist/index.js
@@ -1,3 +1,11 @@
 let numbers = [1, 2, 3];
 let strings = ["a", "b", "C"];
 let mix = [1, "b", 2];
+// let tuple: [string, number] = ["apples", 3, 100];
+let tuple = ["apples", 3];
+console.log(tuple[0]);
+let tuple2 = ["apples", "oranges", 100];
+tuple2.forEach((v) => {
+    if (typeof v === "string")
+        console.log(`String: ${v}`);
+});
--- 04 Arrays Tuples and Enums/start/src/index.ts
+++ 04 Arrays Tuples and Enums/start/src/index.ts
@@ -1,3 +1,12 @@
 let numbers: number[] = [1, 2, 3];
 let strings: string[] = ["a", "b", "C"];
 let mix: (number | string)[] = [1, "b", 2];
+
+// let tuple: [string, number] = ["apples", 3, 100];
+let tuple: [string, number] = ["apples", 3];
+console.log(tuple[0]);
+
+let tuple2: [string, string, number] = ["apples", "oranges", 100];
+tuple2.forEach((v: string | number) => {
+    if (typeof v === "string") console.log(`String: ${v}`);
+});
