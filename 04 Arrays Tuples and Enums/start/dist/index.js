7ac9f70e1427db2f9be6a7bd70791dea71752743
voja <voja@gmail.com>
2021-11-08 09:19:56 +0100

    04 Working with enums

--- 04 Arrays Tuples and Enums/start/dist/index.js
+++ 04 Arrays Tuples and Enums/start/dist/index.js
@@ -9,3 +9,38 @@ tuple2.forEach((v) => {
     if (typeof v === "string")
         console.log(`String: ${v}`);
 });
+var Fruits;
+(function (Fruits) {
+    Fruits[Fruits["Apples"] = 0] = "Apples";
+    // Oranges,
+    Fruits[Fruits["Oranges"] = 30] = "Oranges";
+    Fruits[Fruits["Bananas"] = 31] = "Bananas";
+})(Fruits || (Fruits = {}));
+let products = [
+    [Fruits.Apples, 1],
+    [Fruits.Oranges, 1.5],
+    [Fruits.Bananas, 2.5],
+];
+products.forEach((fruit) => {
+    switch (fruit[0]) {
+        case Fruits.Apples:
+            console.log(`Price of apples is: ${fruit[1]}`);
+            break;
+        case Fruits.Oranges:
+            console.log(`Price of oranges is: ${fruit[1]}`);
+            break;
+        case Fruits.Bananas:
+            console.log(`Price of bananas is: ${fruit[1]}`);
+            break;
+    }
+});
+console.log(Fruits.Apples);
+console.log(Fruits.Oranges);
+console.log(Fruits.Bananas);
+var FruitStrings;
+(function (FruitStrings) {
+    FruitStrings["Apples"] = "Apples";
+    FruitStrings["Oranges"] = "Oranges";
+    FruitStrings["Bananas"] = "Bananas";
+})(FruitStrings || (FruitStrings = {}));
+console.log(FruitStrings.Apples);
--- 04 Arrays Tuples and Enums/start/src/index.ts
+++ 04 Arrays Tuples and Enums/start/src/index.ts
@@ -10,3 +10,41 @@ let tuple2: [string, string, number] = ["apples", "oranges", 100];
 tuple2.forEach((v: string | number) => {
     if (typeof v === "string") console.log(`String: ${v}`);
 });
+
+enum Fruits {
+    Apples,
+    // Oranges,
+    Oranges = 30,
+    Bananas,
+}
+
+let products: [Fruits, number][] = [
+    [Fruits.Apples, 1],
+    [Fruits.Oranges, 1.5],
+    [Fruits.Bananas, 2.5],
+];
+
+products.forEach((fruit: [Fruits, number]) => {
+    switch (fruit[0]) {
+        case Fruits.Apples:
+            console.log(`Price of apples is: ${fruit[1]}`);
+            break;
+        case Fruits.Oranges:
+            console.log(`Price of oranges is: ${fruit[1]}`);
+            break;
+        case Fruits.Bananas:
+            console.log(`Price of bananas is: ${fruit[1]}`);
+            break;
+    }
+});
+
+console.log(Fruits.Apples);
+console.log(Fruits.Oranges);
+console.log(Fruits.Bananas);
+
+enum FruitStrings {
+    Apples = "Apples",
+    Oranges = "Oranges",
+    Bananas = "Bananas",
+}
+console.log(FruitStrings.Apples);
