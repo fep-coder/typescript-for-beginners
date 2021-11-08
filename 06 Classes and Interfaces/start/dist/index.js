ddd57d8d2bcdb3887039bd1dc5b11d9da90b8445
voja <voja@gmail.com>
2021-11-08 10:46:42 +0100

    06 Access control keywords

---
+++ 06 Classes and Interfaces/start/dist/index.js
@@ -0,0 +1,14 @@
+class Person {
+    constructor(id, name, job, salary) {
+        this.id = id;
+        this.name = name;
+        this.job = job;
+        this.salary = salary;
+    }
+    printPerson() {
+        console.log(`${this.name} is a ${this.job} and has a salary of ${this.salary}`);
+    }
+}
+const person = new Person(1, "John", "programmer", 5000);
+person.printPerson();
+// console.log(person.salary);
--- 06 Classes and Interfaces/start/src/index.ts
+++ 06 Classes and Interfaces/start/src/index.ts
@@ -1,1 +1,23 @@
-console.log("Hello, TypeScript");
+class Person {
+    public id: number;
+    public name: string;
+    public job: string;
+    private salary: number;
+
+    constructor(id: number, name: string, job: string, salary: number) {
+        this.id = id;
+        this.name = name;
+        this.job = job;
+        this.salary = salary;
+    }
+
+    printPerson(): void {
+        console.log(
+            `${this.name} is a ${this.job} and has a salary of ${this.salary}`
+        );
+    }
+}
+
+const person = new Person(1, "John", "programmer", 5000);
+person.printPerson();
+// console.log(person.salary);
