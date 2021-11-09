54272c7be9511ba532a3c1eefb0dabb4040bc15a
voja <voja@gmail.com>
2021-11-09 10:48:34 +0100

    07 Constraining generic types

--- 07 Generics/start/dist/index.js
+++ 07 Generics/start/dist/index.js
@@ -4,6 +4,7 @@ const dataTypes_1 = require("./dataTypes");
 let people = [new dataTypes_1.Person("Bob", 32), new dataTypes_1.Person("John", 42)];
 let products = [new dataTypes_1.Product("White shirt", 3), new dataTypes_1.Product("Black shirt", 5)];
 // [...people, ...products].forEach((item) => console.log(`Item: ${item.name}`));
+// class DataCollection<T extends Person | Product> {
 class DataCollection {
     constructor(initialItems) {
         this.items = [];
@@ -12,18 +13,18 @@ class DataCollection {
     add(newItem) {
         this.items.push(newItem);
     }
-    // getNames(): string[] {
-    //     return this.items.map((item) => item.name);
-    // }
+    getNames() {
+        return this.items.map((item) => item.name);
+    }
     getItem(index) {
         return this.items[index];
     }
 }
 let peopleData = new DataCollection(people);
-// console.log(`Names: ${peopleData.getNames().join(", ")}`);
+console.log(`Names: ${peopleData.getNames().join(", ")}`);
 let firstPerson = peopleData.getItem(0);
 console.log(`First person: ${firstPerson.name}, ${firstPerson.age}`);
 let productData = new DataCollection(products);
-// console.log(`Names: ${peopleData.getNames().join(", ")}`);
+console.log(`Product Names: ${productData.getNames().join(", ")}`);
 let firstProduct = productData.getItem(0);
 console.log(`First product: ${firstProduct.name}, ${firstProduct.price}`);
--- 07 Generics/start/src/index.ts
+++ 07 Generics/start/src/index.ts
@@ -4,7 +4,8 @@ let people = [new Person("Bob", 32), new Person("John", 42)];
 let products = [new Product("White shirt", 3), new Product("Black shirt", 5)];
 // [...people, ...products].forEach((item) => console.log(`Item: ${item.name}`));
 
-class DataCollection<T> {
+// class DataCollection<T extends Person | Product> {
+class DataCollection<T extends { name: string }> {
     private items: T[] = [];
 
     constructor(initialItems: T[]) {
@@ -15,9 +16,9 @@ class DataCollection<T> {
         this.items.push(newItem);
     }
 
-    // getNames(): string[] {
-    //     return this.items.map((item) => item.name);
-    // }
+    getNames(): string[] {
+        return this.items.map((item) => item.name);
+    }
 
     getItem(index: number): T {
         return this.items[index];
@@ -25,11 +26,11 @@ class DataCollection<T> {
 }
 
 let peopleData = new DataCollection<Person>(people);
-// console.log(`Names: ${peopleData.getNames().join(", ")}`);
+console.log(`Names: ${peopleData.getNames().join(", ")}`);
 let firstPerson = peopleData.getItem(0);
 console.log(`First person: ${firstPerson.name}, ${firstPerson.age}`);
 
 let productData = new DataCollection<Product>(products);
-// console.log(`Names: ${peopleData.getNames().join(", ")}`);
+console.log(`Product Names: ${productData.getNames().join(", ")}`);
 let firstProduct = productData.getItem(0);
 console.log(`First product: ${firstProduct.name}, ${firstProduct.price}`);
