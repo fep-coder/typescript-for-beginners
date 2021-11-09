5e136356c6a2cd84aa0e0acbc1ce9bae6c322dc5
voja <voja@gmail.com>
2021-11-09 09:54:06 +0100

    06 Extending interfaces

--- 06 Classes and Interfaces/start/src/index.ts
+++ 06 Classes and Interfaces/start/src/index.ts
@@ -3,6 +3,11 @@ interface Animal {
     getDetails(): string;
 }
 
+interface WildAnimal extends Animal {
+    kind: string;
+    getMoreDetails(): string;
+}
+
 class Cat implements Animal {
     constructor(public name: string, public speech: string) {}
 
@@ -19,6 +24,24 @@ class Dog implements Animal {
     }
 }
 
-let animals: Animal[] = [new Cat("Oska", "meows"), new Dog("Loki", "barks")];
+class Lion implements WildAnimal {
+    constructor(
+        public name: string,
+        public speech: string,
+        public kind: string
+    ) {}
 
+    getDetails(): string {
+        return `${this.name} the ${this.kind} ${this.speech}`;
+    }
+
+    getMoreDetails(): string {
+        return `A ${this.kind} is a dangerous wild animal.`;
+    }
+}
+
+let animals: Animal[] = [new Cat("Oska", "meows"), new Dog("Loki", "barks"), new Lion("Leo", "roars", "lion")];
 animals.forEach((animal) => console.log(animal.getDetails()));
+
+let wildAnimals: WildAnimal[] = [new Lion("Leo", "roars", "lion")];
+wildAnimals.forEach((animal) => console.log(animal.getMoreDetails()));
