b35569719291e47fa5817cd9a620cb34b8732427
voja <voja@gmail.com>
2021-11-10 10:09:01 +0100

    08 Using generic collections

--- 08 Generics with collections and iterators/start/dist/index.js
+++ 08 Generics with collections and iterators/start/dist/index.js
@@ -1,22 +1,22 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const dataTypes_1 = require("./dataTypes");
-let products = [new dataTypes_1.Product("White shirt", 3), new dataTypes_1.Product("Black shirt", 5)];
+const Product_1 = require("./Product");
+let products = [new Product_1.Product("White shirt", 3), new Product_1.Product("Black shirt", 5)];
 class Collection {
-    constructor(items = []) {
-        this.items = items;
+    constructor(initialItems = []) {
+        this.items = new Set(initialItems);
     }
     add(...newItems) {
-        this.items.push(...newItems);
+        newItems.forEach((newItem) => this.items.add(newItem));
     }
     get(name) {
-        return this.items.find((item) => item.name === name);
+        return [...this.items.values()].find((item) => item.name === name);
     }
     get count() {
-        return this.items.length;
+        return this.items.size;
     }
 }
 let productCollection = new Collection(products);
-console.log(`There are ${productCollection.count} products`);
+console.log(`There are ${productCollection.count} products.`);
 let p = productCollection.get("White shirt");
 console.log(`Product: ${p.name}, ${p.price}`);
--- 08 Generics with collections and iterators/start/src/index.ts
+++ 08 Generics with collections and iterators/start/src/index.ts
@@ -1,26 +1,29 @@
-import { Product } from "./dataTypes";
+import { Product } from "./Product";
 
 let products = [new Product("White shirt", 3), new Product("Black shirt", 5)];
 
-type shapeType = { name: string };
+class Collection<T extends { name: string }> {
+    private items: Set<T>;
 
-class Collection<T extends shapeType> {
-    constructor(private items: T[] = []) {}
+    constructor(initialItems: T[] = []) {
+        this.items = new Set<T>(initialItems);
+    }
 
     add(...newItems: T[]): void {
-        this.items.push(...newItems);
+        newItems.forEach((newItem) => this.items.add(newItem));
     }
 
     get(name: string): T {
-        return this.items.find((item) => item.name === name);
+        return [...this.items.values()].find((item) => item.name === name);
     }
 
     get count(): number {
-        return this.items.length;
+        return this.items.size;
     }
 }
 
 let productCollection: Collection<Product> = new Collection(products);
-console.log(`There are ${ productCollection.count } products`);
+console.log(`There are ${productCollection.count} products.`);
+
 let p = productCollection.get("White shirt");
-console.log(`Product: ${ p.name }, ${ p.price }`);
+console.log(`Product: ${p.name}, ${p.price}`);
